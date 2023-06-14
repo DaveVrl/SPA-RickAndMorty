71dffaba5eb04a42a6399c96311de2ba6d74b2b4
Dave <doble.aguila@hotmail.com>
2023-06-14 15:30:11 -0300

    Integrador Promises

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,18 +1,13 @@
 const http = require("http");
-const data = require("./utils/data");
+const { getCharById } = require("./controllers/getCharById")
 
 http.createServer((req , res) => {
     
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     if(req.url.includes("/rickandmorty/character")) {
-        const id = req.url.split("/").at(-1)
-
-        const characterFound = data.find((character) => {
-            return character.id === +id
-        })
-
-        return res.writeHead(200 , {"Content-type": "application/json"}).end(JSON.stringify(characterFound))
+        const id = req.url.split("/").at(-1);
+        getCharById(res , +id);  //id a number
     }
 
 }).listen(3001)
